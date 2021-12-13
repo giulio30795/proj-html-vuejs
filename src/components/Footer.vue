3cd473e8a44655833384ce6dfe6ced76a966ed33
giulio30795 <giulioincarbone95@gmail.com>
2021-12-13 15:29:33 +0100

    Social

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,132 +1,139 @@
 <template>
-    <section>
-        <div class="container py-5">
-            <div class="row justify-content-center mx-2s">
-                <div class="col-2">
-                    <h5>About Us</h5>
-                    <p>Elipsis magna a nulla elite forte maecenas est elementum magna etos interdum movum morbi vehicula morbi. </p>
-                    <img class="logo" src="../assets/images/logo_footer.png" alt="">
-                </div>
-                <div class="col-3 px-4">
-                    <h5>Archives</h5>
-                    <div class="month">October 2014 (2)</div>
-                    <div class="month">September 2014 (2)</div>
-                    <div class="month">August 2014 (2)</div>
-                    <div class="month">July 2014 (2)</div>
-                    <div class="month">June 2014 (2)</div>
-                    <div class="month">May 2014 (1)</div>
-                </div>
-                <div class="col-3 px-4">
-                    <h5>Our Features</h5>
-                    <p>Nunc gravida rutrum mauris vehin hasellus ac pretium augue vivamul lacus ligul mattis ac vene: </p>
-                    <ul class="list-unstyled">
-                        <li class="d-flex align-items-center my-2">
-                            <img
-                                src="../assets/images/style_4_alt.png"
-                                alt="check"
-                            />
-                            <div class="ms-2">
-                                Friendly and welcoming place
-                            </div>
-                        </li>
-                        <li class="d-flex align-items-center my-2">
-                            <img
-                                src="../assets/images/style_4_alt.png"
-                                alt="check"
-                            />
-                            <div class="ms-2">
-                                Science and art classes
-                            </div>
-                        </li>
-                        <li class="d-flex align-items-center my-2">
-                            <img
-                                src="../assets/images/style_4_alt.png"
-                                alt="check"
-                            />
-                            <div class="ms-2">
-                                Positive learning environment
-                            </div>
-                        </li>
-                        <li class="d-flex align-items-center my-2">
-                            <img
-                                src="../assets/images/style_4_alt.png"
-                                alt="check"
-                            />
-                            <div class="ms-2">
-                                Educational field trips
-                            </div>
-                        </li>
-                        <li class="d-flex align-items-center my-2">
-                            <img
-                                src="../assets/images/style_4_alt.png"
-                                alt="check"
-                            />
-                            <div class="ms-2">
-                                Writing and reading classes
-                            </div>
-                        </li>
-                        <li class="d-flex align-items-center my-2">
-                            <img
-                                src="../assets/images/style_4_alt.png"
-                                alt="check"
-                            />
-                            <div class="ms-2">
-                                Science and art classes
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-3 px-4">
-                    <h5>Recent Post</h5>
-                    <div class="d-flex justify-content-center align-items-center">
-                        <div
-                            class="img-container d-flex justify-content-center align-items-center"
-                        >
-                            <img src="../assets/images/blog_09-150x150.jpg" alt="clock" />
-                        </div>
-                        <div>
-                            <h6>Drawing and Painting Lessons</h6>
-                            <span> <img src="../assets/images/date_footer.png" alt=""></span>
-                            <span>October 03, 2014</span>
-                        </div>
+    <div>
+        <section>
+            <div class="container py-5">
+                <div class="row justify-content-center mx-4">
+                    <div class="col-3 px-4">
+                        <h5>About Us</h5>
+                        <p>Elipsis magna a nulla elite forte maecenas est elementum magna etos interdum movum morbi vehicula morbi. </p>
+                        <img class="logo" src="../assets/images/logo_footer.png" alt="">
                     </div>
-
-                    <div class="d-flex justify-content-center align-items-center">
-                        <div
-                            class="img-container d-flex justify-content-center align-items-center"
-                        >
-                            <img src="../assets/images/blog_10-150x150.jpg" alt="clock" />
-                        </div>
-                        <div>
-                            <h6>Fall Parents Meeting Day</h6>
-                            <span> <img src="../assets/images/date_footer.png" alt=""></span>
-                            <span>October 03, 2014</span>
-
-                        </div>
+                    <div class="col-3 px-4">
+                        <h5>Archives</h5>
+                        <div class="month">October 2014 (2)</div>
+                        <div class="month">September 2014 (2)</div>
+                        <div class="month">August 2014 (2)</div>
+                        <div class="month">July 2014 (2)</div>
+                        <div class="month">June 2014 (2)</div>
+                        <div class="month">May 2014 (1)</div>
                     </div>
-
-                    <div class="d-flex justify-content-center align-items-center">
-                        <div
-                            class="img-container d-flex justify-content-center align-items-center"
-                        >
-                            <img src="../assets/images/blog_08-150x150.jpg" alt="clock" />
-                        </div>
-                        <div>
-                            <h6>Birthday in Kindergarten</h6>
-                            <span> <img src="../assets/images/date_footer.png" alt=""></span>
-                            <span>september 20, 2014</span>
-                        </div>
+                    <div class="col-3 px-4">
+                        <h5>Our Features</h5>
+                        <p>Nunc gravida rutrum mauris vehin hasellus ac pretium augue vivamul lacus ligul mattis ac vene: </p>
+                        <ul class="list-unstyled">
+                            <li class="d-flex align-items-center my-2">
+                                <img
+                                    src="../assets/images/style_4_alt.png"
+                                    alt="check"
+                                />
+                                <div class="ms-2">
+                                    Friendly and welcoming place
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center my-2">
+                                <img
+                                    src="../assets/images/style_4_alt.png"
+                                    alt="check"
+                                />
+                                <div class="ms-2">
+                                    Science and art classes
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center my-2">
+                                <img
+                                    src="../assets/images/style_4_alt.png"
+                                    alt="check"
+                                />
+                                <div class="ms-2">
+                                    Positive learning environment
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center my-2">
+                                <img
+                                    src="../assets/images/style_4_alt.png"
+                                    alt="check"
+                                />
+                                <div class="ms-2">
+                                    Educational field trips
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center my-2">
+                                <img
+                                    src="../assets/images/style_4_alt.png"
+                                    alt="check"
+                                />
+                                <div class="ms-2">
+                                    Writing and reading classes
+                                </div>
+                            </li>
+                            <li class="d-flex align-items-center my-2">
+                                <img
+                                    src="../assets/images/style_4_alt.png"
+                                    alt="check"
+                                />
+                                <div class="ms-2">
+                                    Science and art classes
+                                </div>
+                            </li>
+                        </ul>
                     </div>
+                    <div class="col-3 px-4">
+                        <h5>Recent Post</h5>
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div
+                                class="img-container d-flex justify-content-center align-items-center"
+                            >
+                                <img src="../assets/images/blog_09-150x150.jpg" alt="clock" />
+                            </div>
+                            <div>
+                                <h6>Drawing and Painting Lessons</h6>
+                                <span> <img src="../assets/images/date_footer.png" alt=""></span>
+                                <span>October 03, 2014</span>
+                            </div>
+                        </div>
 
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div
+                                class="img-container d-flex justify-content-center align-items-center"
+                            >
+                                <img src="../assets/images/blog_10-150x150.jpg" alt="clock" />
+                            </div>
+                            <div>
+                                <h6>Fall Parents Meeting Day</h6>
+                                <span> <img src="../assets/images/date_footer.png" alt=""></span>
+                                <span>October 03, 2014</span>
+
+                            </div>
+                        </div>
+
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div
+                                class="img-container d-flex justify-content-center align-items-center"
+                            >
+                                <img src="../assets/images/blog_08-150x150.jpg" alt="clock" />
+                            </div>
+                            <div>
+                                <h6>Birthday in Kindergarten</h6>
+                                <span> <img src="../assets/images/date_footer.png" alt=""></span>
+                                <span>september 20, 2014</span>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <Social />
+    </div>
 </template>
 
 <script>
+import Social from './Social.vue'
 export default {
-name: 'Footer'
+name: 'Footer',
+components:{
+    Social,
+}
 }
 </script>
 
@@ -137,7 +144,7 @@ section{
      color: $footer-color;
 }
 .logo{
-    max-width: 100%;
+    max-width: 75%;
 }
 p, li, span{
     font-size: .8rem;
@@ -173,7 +180,7 @@ h6{
     }
 }
 div:last-child span{
-    margin-right: 10px;
+    margin-right: 5px;
 }
 
 </style>
---
+++ src/components/Social.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container  d-flex justify-content-center flex-column align-itemr-center py-4">
+        <div class="d-flex justify-content-center mb-3">
+            <span class="d-flex justify-content-center align-items-center">
+                <img src="../assets/images/behance.png" alt="Teacher">
+            </span>
+            <span class="d-flex justify-content-center align-items-center">
+                <img src="../assets/images/dribbble.png" alt="Teacher">
+            </span>
+            <span class="d-flex justify-content-center align-items-center">
+                <img src="../assets/images/envato.png" alt="Teacher">
+            </span>
+            <span class="d-flex justify-content-center align-items-center">
+                <img src="../assets/images/facebook.png" alt="Teacher">
+            </span>
+            <span class="d-flex justify-content-center align-items-center">
+                <img src="../assets/images/twitter.png" alt="Teacher">
+            </span>
+        </div>
+        <div class="text-center">
+            Fable - Children Kindergarten WordPress Theme by QuanticaLabs
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'Social'
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/style/variables.scss";
+
+
+span{
+    background-color: $main;
+    margin: 0 1px;
+    width: 50px;
+    height: 50px;
+}
+
+div:last-child div {
+    color: $text-grey;
+}
+</style>
